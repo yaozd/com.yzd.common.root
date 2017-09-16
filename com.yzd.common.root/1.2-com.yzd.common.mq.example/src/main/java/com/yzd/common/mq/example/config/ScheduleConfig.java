be7f76d0adec83e21130fd6f2fa82dbcd260358d
yzd <tedijoteamo@163.com>
2017-09-16 17:37:14 +0800

    Executors.newScheduledThreadPool(30);一样没有根本的区别只是自定义线程池的名字

--- com.yzd.common.root/1.2-com.yzd.common.mq.example/src/main/java/com/yzd/common/mq/example/config/ScheduleConfig.java
+++ com.yzd.common.root/1.2-com.yzd.common.mq.example/src/main/java/com/yzd/common/mq/example/config/ScheduleConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 /**
  * Created by zd.yao on 2017/9/14.
@@ -24,6 +24,13 @@ public class ScheduleConfig implements SchedulingConfigurer {
     @Bean(destroyMethod="shutdown")
     public Executor taskExecutor() {
         //可以根据调度任务的个数来设置线程池的大小
-        return Executors.newScheduledThreadPool(30);
+        //return Executors.newScheduledThreadPool(30);
+        //Executors.newScheduledThreadPool(30);一样没有根本的区别只是自定义线程池的名字
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(30);
+        scheduler.setThreadNamePrefix("TaskScheduler-");
+        scheduler.setAwaitTerminationSeconds(60);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return scheduler;
     }
 }
