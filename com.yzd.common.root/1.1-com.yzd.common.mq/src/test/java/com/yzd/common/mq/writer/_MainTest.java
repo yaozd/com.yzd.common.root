87c6634ddc9bd203d6df1262fc37c8032843fdbc
yaozd <tedijoteamo@163.com>
2019-07-24 17:59:05 +0800

    并发写入测试

--- com.yzd.common.root/1.1-com.yzd.common.mq/src/test/java/com/yzd/common/mq/writer/_MainTest.java
+++ com.yzd.common.root/1.1-com.yzd.common.mq/src/test/java/com/yzd/common/mq/writer/_MainTest.java
@@ -4,25 +4,32 @@ import com.yzd.common.mq.enumExt.JobLockEnum;
 import com.yzd.common.mq.redis.job.enumExt.JobEnum;
 import com.yzd.common.mq.redis.job.lock.RedisJobLockUtil;
 import com.yzd.common.mq.redis.job.writer.RedisJobWriterUtil;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zd.yao on 2017/8/29.
  */
 public class _MainTest {
-
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
     JobEnum keyEnum=JobLockEnum.HelloWorldJob;
 
     /**
+     * 并发写入测试
      * 消息队列-写入消息
      */
     @Test
+    @PerfTest(threads = 100,invocations=10)
     public void writerExample(){
 
-        for (int i = 0; i < 200; i++) {
-            String val="id=" + i;
+        for (int i = 0; i < 10000000; i++) {
+            String val="id=" + i+UUID.randomUUID().toString();
             RedisJobWriterUtil.write(keyEnum,val);
         }
     }
